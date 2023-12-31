2d08c0b109a71cdbcbccb3e17e2fe4e10ff341b7
joaosnet <87316339+joaosnet@users.noreply.github.com>
2023-12-31 18:13:00 -0300

    Remove unused images and update logo

--- dashapp/views.py
+++ dashapp/views.py
@@ -4,11 +4,13 @@ from dashapp import app, server, database, bcrypt
 from dashapp.models import Usuario
 from flask_login import login_required, login_user, logout_user, current_user
 from graf import WheelOfLife
-from flask import render_template, url_for, redirect
+from flask import render_template, url_for, redirect, request
 from dashapp.forms import FormLogin, FormCriarConta
 import os
 from werkzeug.utils import secure_filename
 import numpy as np
+import pandas as pd
+import joblib
 
 # Cria uma instância da classe WheelOfLife
 wheel = WheelOfLife()
@@ -298,3 +300,70 @@ def criarconta():
 def logout():
     logout_user()
     return redirect(url_for("login"))
+
+@server.route('/previsao', methods=['GET', 'POST'])
+def previsao():
+    from pprint import pprint as pp
+    dicionario = {}
+    if request.method == 'POST':
+        # Obtém os dados do formulário
+        data = request.form
+
+        # verifica se todos os campos foram preenchidos
+        # x_numericos = {'latitude': 0, 'longitude': 0, 'accommodates': 0, 'bathrooms': 0, 'bedrooms': 0, 'beds': 0, 'extra_people': 0,
+        #        'minimum_nights': 0, 'ano': 0, 'mes': 0, 'n_amenities': 0, 'host_listings_count': 0}
+
+        # x_tf = {'host_is_superhost': 0, 'instant_bookable': 0}
+
+        # x_listas = {'property_type': ['Apartment', 'Bed and breakfast', 'Condominium', 'Guest suite', 'Guesthouse', 'Hostel', 'House', 'Loft', 'Outros', 'Serviced apartment'],
+        #             'room_type': ['Entire home/apt', 'Hotel room', 'Private room', 'Shared room'],
+        #             'cancellation_policy': ['flexible', 'moderate', 'strict', 'strict_14_with_grace_period']
+        #             }
+        # for item in x_listas:  # Para cada item nas listas de x_listas
+        #     for valor in x_listas[item]:  # Para cada valor no item atual
+        #         dicionario[f'{item}_{valor}'] = 0  # Adiciona uma chave-valor ao dicionário
+
+        # for item in x_numericos:  # Para cada item nos valores numéricos
+        #     if item == 'latitude' or item == 'longitude':  # Se o item for latitude ou longitude
+        #         valor = st.number_input(f'{item}', step=0.00001, value=0.0, format="%.5f")  # Lê um número de entrada do usuário
+        #     elif item == 'extra_people':  # Se o item for extra_people
+        #         valor = st.number_input(f'{item}', step=0.01, value=0.0)  # Lê um número de entrada do usuário
+        #     else:
+        #         valor = st.number_input(f'{item}', step=1, value=0)  # Lê um número de entrada do usuário
+        #     x_numericos[item] = valor  # Atualiza o valor no dicionário x_numericos
+
+        # for item in x_tf:  # Para cada item nos valores booleanos
+        #     valor = st.selectbox(f'{item}', ('Sim', 'Não'))  # Lê uma opção de seleção do usuário
+        #     if valor == "Sim":  # Se o valor for "Sim"
+        #         x_tf[item] = 1  # Atualiza o valor no dicionário x_tf para 1
+        #     else:
+        #         x_tf[item] = 0  # Atualiza o valor no dicionário x_tf para 0
+
+        # for item in x_listas:  # Para cada item nas listas de x_listas
+        #     valor = st.selectbox(f'{item}', x_listas[item])  # Lê uma opção de seleção do usuário
+        #     dicionario[f'{item}_{valor}'] = 1  # Atualiza o valor no dicionário dicionario para 1
+
+        # Atualiza o dicionário com os valores do formulário
+        for key, value in data.items():
+            try:
+                dicionario[key] = float(value)
+            except ValueError:
+                dicionario[key] = value
+
+        # Atualiza o dicionário com os valores do formulário
+        # dicionario.update({key: float(value) for key, value in data.items()})
+
+        # Cria um DataFrame a partir do dicionário
+        valores_x = pd.DataFrame(dicionario, index=[0])
+
+        # Carrega o modelo de aprendizado de máquina pré-treinado
+        modelo = joblib.load(r'C:\Users\joaod\Documents\GitHub\Data-Science-Project\modelo.joblib')
+
+        # Faz a previsão do valor da propriedade com base nos valores de entrada fornecidos pelo usuário
+        try:
+            valor_predito = modelo.predict(valores_x)[0]  # Faz a previsão do valor da propriedade
+            return render_template('previsao.html', valor_predito=f'O valor previsto da propriedade é R$ {valor_predito:.2f}')  # Exibe o valor previsto com duas casas decimais
+        except Exception as e:
+            return render_template('previsao.html', error=f'Ocorreu um erro ao prever o valor da propriedade: {e}')  # Exibe uma mensagem de erro se ocorrer algum problema
+    else:
+        return render_template('previsao.html')
